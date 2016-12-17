331b380dd5d7c17f47d1a127c9ed674a7ae88724
outlandkarasu <outland.karasu@gmail.com>
2016-12-17 17:16:17 +0900

    add namespace.

--- Assets/Stereoscopic4D/Scripts/Transform4D.cs
+++ Assets/Stereoscopic4D/Scripts/Transform4D.cs
@@ -2,61 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// This class extends GameObject to 4D transform
-/// </summary>
-public class Transform4D : MonoBehaviour {
+namespace Stereoscopic4D {
 
 	/// <summary>
-	/// w position.
+	/// This class extends GameObject to 4D transform
 	/// </summary>
-	public float w;
+	public class Transform4D : MonoBehaviour {
 
-	[Header("Rotation with W")]
+		/// <summary>
+		/// w position.
+		/// </summary>
+		public float w;
 
-	/// <summary>
-	/// The XZ rotation.
-	/// </summary>
-	public float xz;
+		[Header("Rotation with W")]
 
-	/// <summary>
-	/// The YZ rotation.
-	/// </summary>
-	public float yz;
+		/// <summary>
+		/// The XZ rotation.
+		/// </summary>
+		public float xz;
 
-	/// <summary>
-	/// The XY rotation.
-	/// </summary>
-	public float xy;
+		/// <summary>
+		/// The YZ rotation.
+		/// </summary>
+		public float yz;
 
-	/// <summary>
-	/// Gets or sets the 4D position.
-	/// </summary>
-	/// <value>The 4D position.</value>
-	public Vector4 position {
-		get {
-			Vector3 pos = transform.position;
-			return new Vector4 (pos.x, pos.y, pos.z, w);
+		/// <summary>
+		/// The XY rotation.
+		/// </summary>
+		public float xy;
+
+		/// <summary>
+		/// Gets or sets the 4D position.
+		/// </summary>
+		/// <value>The 4D position.</value>
+		public Vector4 position {
+			get {
+				Vector3 pos = transform.position;
+				return new Vector4 (pos.x, pos.y, pos.z, w);
+			}
+			set {
+				transform.position = new Vector3 (value.x, value.y, value.z);
+				w = value.w;
+			}
 		}
-		set {
-			transform.position = new Vector3 (value.x, value.y, value.z);
-			w = value.w;
-		}
-	}
 
-	/// <summary>
-	/// Gets or sets the world rotation with w.
-	/// </summary>
-	/// <value>The local rotation with w.</value>
-	public Quaternion rotationWithW {
-		get {
-			return Quaternion.Euler (xz, yz, xy);
-		}
-		set {
-			Vector3 angles = value.eulerAngles;
-			xz = angles.x;
-			yz = angles.y;
-			xy = angles.z;
+		/// <summary>
+		/// Gets or sets the world rotation with w.
+		/// </summary>
+		/// <value>The local rotation with w.</value>
+		public Quaternion rotationWithW {
+			get {
+				return Quaternion.Euler (xz, yz, xy);
+			}
+			set {
+				Vector3 angles = value.eulerAngles;
+				xz = angles.x;
+				yz = angles.y;
+				xy = angles.z;
+			}
 		}
 	}
 }
