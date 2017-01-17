777ed69045c24551b76b85363c51f326981170bb
miurror <miurror.jp@gmail.com>
2017-01-17 22:41:04 +0900

    Rename a script

---
+++ Assets/Stereoscopic4D/Scripts/Camera4D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stereoscopic4D {
+
+	/// <summary>
+	/// Camera 4D controller.
+	/// </summary>
+	public class Camera4D : MonoBehaviour {
+
+		/// <summary>
+		/// The enable4 D stereoscopic flag.
+		/// </summary>
+		public bool enable4DStereoscopic = true;
+
+		/// <summary>
+		/// The squint factor.
+		/// </summary>
+		public float squintFactor = 1.0f;
+	}
+}
